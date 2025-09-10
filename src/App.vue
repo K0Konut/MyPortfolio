4d0f779011ae0af19348e9d1fd618953bbe388f2
Costa <maskulov@et.esiea.fr>
2025-09-10 17:45:19 +0200

    rework analytics

--- src/App.vue
+++ src/App.vue
@@ -3,12 +3,10 @@
     <Header />
     <router-view />
     <Footer />
-    <Analytics />
   </div>
 </template>
 
 <script setup>
 import Header from './components/Header.vue';
 import Footer from './components/Footer.vue';
-import { Analytics } from "@vercel/analytics/next"
 </script>
